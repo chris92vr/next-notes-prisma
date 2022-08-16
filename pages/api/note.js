a60d1225af76bb03a78b651ca7642708f936ebae
chris92vr <garofolichristian@gmail.com>
2022-08-16 16:20:14 +0200

    Added updateNote

--- pages/api/note.js
+++ pages/api/note.js
@@ -1,5 +1,6 @@
 import { createNote } from '../../prisma/Note';
 import { getSession } from 'next-auth/react';
+
 export default async function handle(req, res) {
   // Get the current session data with {user, email, id}
   const session = await getSession({ req });
@@ -11,6 +12,14 @@ export default async function handle(req, res) {
     // also pass the session which would be use to get the user information
     const note = await createNote(title, body, session);
     // return created note
+    return res.json(note);
+  } else if (req.method == 'PUT') {
+    const { id, title, body } = req.body;
+    // const updatedData = {title, body}
+    // Update current note
+    // also pass the session which would be use to get the user information
+    const note = await updateNote(id, { title, body }, session);
+    // return updated note
     return res.json(note);
   }
 }
